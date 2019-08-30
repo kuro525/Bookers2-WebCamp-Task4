e9ee41b88fc94c5302d7af7871590add131cd1ce
kuro525 <deep.impact525-2012-5-pc@hotmail.co.jp>
2019-08-31 01:24:23 +0900

    [Add] ログイン処理

---
+++ app/models/user.rb
@@ -0,0 +1,16 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  validates :name, presence: true, length: {minimum: 2 ,maximum: 20}
+
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  def email_required?
+    false
+  end
+
+  def email_changed?
+    false
+  end
+end
